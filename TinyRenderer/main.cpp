9a771a241d9d4d7a575165d2cc90d260b5029a8a
jwgsbbc <james.shephard@bbc.co.uk>
2017-05-17 15:08:16 +0100

    adding objects and adding test loading

--- TinyRenderer/main.cpp
+++ TinyRenderer/main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include "geometry.h"
 #include "tgaimage.h"
+#include "model.h"
 
 int main(int argc, const char * argv[]) {
     
+    Model model("obj/diablo3_pose/diablo3_pose.obj");
+    
+    std::cout << "loaded model, " << model.nfaces() << " faces\n";
+    
     Vec3f vec(1.0f, 0.0f, 0.0f);
     
-    TGAImage image(100, 100, 4);
+    TGAImage image(256, 256, 4);
     
     for(int x=0; x<image.get_width(); x++) {
         for (int y=0; y<image.get_height(); y++) {
@@ -17,5 +22,6 @@ int main(int argc, const char * argv[]) {
     bool done = image.write_tga_file("image.tga");
     
     std::cout << "Hello, World!\n" << vec << "\n" << ( done ? "done" : "failed" ) << "\n";
+    
     return 0;
 }
